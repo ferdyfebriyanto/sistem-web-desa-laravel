8f3f743c941c931b8effe91d132469d072682952
ferdyfebriyanto <ferdyfebriyanto@gmail.com>
2022-07-14 23:06:34 +0700

    Update routing

--- routes/web.php
+++ routes/web.php
@@ -4,31 +4,27 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PendudukController;
+use App\Http\Controllers\KartuKeluargaController;
+use App\Http\Controllers\KelahiranController;
+use App\Http\Controllers\MeninggalController;
+use App\Http\Controllers\PendatangController;
+use App\Http\Controllers\PindahController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 Route::get('/', ['middleware' =>'guest', function(){
     return view('auth.login');
   }]);
-
 
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::get('/admin/home', [HomeController::class, 'adminHome'])->name('admin-home')->middleware('is_admin');
+Route::get('/admin/home', [HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
 
-Route::get('/admin/penduduk', [PendudukController::class, 'index'])->name('penduduk.index')->middleware('is_admin');
-Route::get('/admin/penduduk/create', [PendudukController::class, 'create'])->name('penduduk.create')->middleware('is_admin');
-
-// Route::prefix('admin')->group(function(){
-//   Route::resource('penduduk', PendudukController::class)->middleware('is_admin');
-// });
+Route::prefix('admin')->group(function(){
+  Route::resource('penduduk', PendudukController::class)->middleware('is_admin');
+  Route::resource('kartukeluarga', KartuKeluargaController::class)->middleware('is_admin');
+  Route::resource('kelahiran', KelahiranController::class)->middleware('is_admin');
+  Route::resource('meninggal', MeninggalController::class)->middleware('is_admin');
+  Route::resource('pendatang', PendatangController::class)->middleware('is_admin');
+  Route::resource('pindah', PindahController::class)->middleware('is_admin');
+});
